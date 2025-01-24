0a3431f218f0eded6506d70bfd9eff00b077e48f
Abbos <abbossolijonov707@gmail.com>
2025-01-24 18:42:41 +0500

    add server users

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 5000;
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
 
-// Middleware
-app.use(express.json()); // Parse incoming JSON requests
+app.use(express.json())
 
-// Routes
-app.use('/api/courses', require('./routes/courses')); // Courses API route
+app.use('/api/courses', require('./routes/courses'))
+app.use('/api/users', require('./routes/users'))
 
-// Root endpoint
 app.get('/', (req, res) => {
-  res.send('Welcome to the Chess Backend API!');
-});
+	res.send('Welcome to the Chess Backend API!')
+})
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+	console.log(`Server is running on port ${PORT}`)
+})
