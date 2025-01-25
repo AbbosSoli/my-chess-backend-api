37a385debe4b824113d22ba57e5a9f3c1c3e5d75
Abbos <abbossolijonov707@gmail.com>
2025-01-25 15:01:57 +0500

    add delete, get, put, post methods

--- routes/courses.js
+++ routes/courses.js
@@ -1,16 +1,57 @@
 const express = require('express')
 const router = express.Router()
+let courses = require('../data/courses.json')
 
-// Get all courses
 router.get('/', (req, res) => {
-	// Send a response here, for example, an array of courses
-	res.json({ message: 'Courses fetched successfully' })
+	res.json(courses)
 })
 
-// Add a new course
+router.get('/:id', (req, res) => {
+	const course = courses.find(c => c.id === req.params.id)
+	if (course) {
+		res.json(course)
+	} else {
+		res.status(404).send('Course not found')
+	}
+})
+
 router.post('/', (req, res) => {
-	// Handle course creation
-	res.json({ message: 'Course added successfully' })
+	const newCourse = req.body
+	if (!newCourse.id || !newCourse.name || !newCourse.description) {
+		return res.status(400).send('Invalid course data')
+	}
+
+	courses.push(newCourse)
+	res.status(201).json(newCourse)
+})
+
+router.put('/:id', (req, res) => {
+	const { id } = req.params
+	const updatedCourse = req.body
+	const courseIndex = courses.findIndex(c => c.id === id)
+
+	if (courseIndex === -1) {
+		return res.status(404).send('Course not found')
+	}
+
+	if (!updatedCourse.name || !updatedCourse.description) {
+		return res.status(400).send('Invalid course data')
+	}
+
+	courses[courseIndex] = { id, ...updatedCourse }
+	res.json(courses[courseIndex])
+})
+
+router.delete('/:id', (req, res) => {
+	const { id } = req.params
+	const courseIndex = courses.findIndex(c => c.id === id)
+
+	if (courseIndex === -1) {
+		return res.status(404).send('Course not found')
+	}
+
+	const deletedCourse = courses.splice(courseIndex, 1)
+	res.json(deletedCourse)
 })
 
 module.exports = router
